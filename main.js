f72c3d82ecf30f2cbd0104352bae96b6080cf1ef
MohamedAmineYousfi <mohamedamineyousfi@gmail.com>
2021-06-25 11:42:05 +0100

    fgfhg

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// $(document).ready(function() {
-//     $("#img1").attr("src", ".asset/img01.jpg");
-//     $("#img2").attr("src", ".asset/img02.jpg");
-//     $("#img3").attr("src", ".asset/img03.jpg");
-// });
+
 
 var images = new Array ();
 images[0] = "asset/img01.jpg";
@@ -12,7 +8,7 @@ images[3] = "asset/img03.jpg";
 var size = images.length
 var x = Math.floor(size*Math.random())
 
-// $('#random').attr('src',image[x]);
+
 
 $('.all').css('background-image', 'url('+ images[x] +')');
 
