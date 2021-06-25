bbd01d645a549ea344ef22f0a9b4a7da8bed45db
dhafer1hmdi <hmididhafer@yahoo.com>
2021-06-25 11:27:41 +0100

    css added

--- main.js
+++ main.js
@@ -38,9 +38,13 @@ function each(coll, f) {
     });
     return acc;
   }
-
-
-
+  var images = new Array ();
+  images[0] = "asset/img01.jpg";
+  images[1] = "asset/img02.jpg";
+  images[2] = "asset/img03.jpg";
+  images[3] = "asset/img03.jpg";
+  var size = images.length
+  var x = Math.floor(size*Math.random())
 
 $('.all').css('background-image', 'url('+ images[x] +')');
 
